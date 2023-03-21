976f7edca18aedc0e3926d64298c43562629d572
Hesam Ossanloo <hesam.ossanloo@gmail.com>
2023-03-21 12:35:11 +0100

    delete and clean old project

--- src/Demo.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Demo from './Demo';
-
-test('renders learn react link', () => {
-  render(<Demo />);
-  const linkElement = screen.getByText(/Volpes Energy/i);
-  expect(linkElement).toBeInTheDocument();
-});
